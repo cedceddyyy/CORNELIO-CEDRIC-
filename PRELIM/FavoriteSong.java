c8ca56b60d67fadd33329807ea4127969ce1aed8
cedceddyyy <148033423+cedceddyyy@users.noreply.github.com>
2023-10-25 06:02:37 +0100

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,6 @@
+public class FavoriteSong {
+ public static void main (String args[]){
+ 
+ System.out.println("Let it go, Let it go \nCan't hold it back anymore \nLet it go, Let it go \nTurn away and slam the door");
+}
+}
